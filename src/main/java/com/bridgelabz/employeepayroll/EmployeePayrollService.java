a20305d2a9dab2e81ffdbe5f631a060120a80f50
Shivak007 <kshivaoffl2824@gmail.com>
2026-04-02 15:53:36 +0530

    [Shiva Kumar] : feat: UC1-add connect java program to payroll database using JDBC

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -1,4 +1,37 @@
 package com.bridgelabz.employeepayroll;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class EmployeePayrollService {
+
+    public static void main(String[] args) {
+
+        String jdbcURL = "jdbc:mysql://localhost:3306/employee_payroll_db";
+        String username = "root";
+        String password = "Shiva@123";
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            System.out.println("Driver Loaded");
+
+            Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+
+            System.out.println("Connection Established Successfully");
+
+        } catch (ClassNotFoundException e) {
+
+            System.out.println("Driver not found");
+
+        } catch (SQLException e) {
+
+            System.out.println("Connection failed");
+            e.printStackTrace();
+
+        }
+
+    }
 }
