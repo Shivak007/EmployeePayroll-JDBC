8512771d446f073b3d28335b756e4e19ece37f94
Shivak007 <kshivaoffl2824@gmail.com>
2026-04-03 17:22:08 +0530

    [Shiva Kumar] : feat: UC5-Retrieve employees by date range

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -1,20 +1,23 @@
-package com.bridgelabz.employeepayroll;
 import java.sql.*;
 
 public class EmployeePayrollService {
     public static void main(String[] args) {
 
-        String query = "UPDATE employee_payroll SET salary = ? WHERE name = ?";
+        String query = "SELECT * FROM employee_payroll WHERE start BETWEEN ? AND ?";
 
         try (Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/employee_payroll_db", "root", "Shiva@123");
              PreparedStatement ps = con.prepareStatement(query)) {
 
-            ps.setDouble(1, 300000);
-            ps.setString(2, "Terisa");
+            ps.setDate(1, Date.valueOf("2023-01-01"));
+            ps.setDate(2, Date.valueOf("2024-12-31"));
 
-            int rows = ps.executeUpdate();
-            System.out.println("Rows Updated: " + rows);
+            ResultSet rs = ps.executeQuery();
+
+            System.out.println("Employees in Date Range:");
+            while (rs.next()) {
+                System.out.println(rs.getString("name"));
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
