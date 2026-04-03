dbe9a4d9c3b471e894e5ee6857472f937da81926
Shivak007 <kshivaoffl2824@gmail.com>
2026-04-03 17:28:16 +0530

    [Shiva Kumar] : feat: UC6-Perform aggregate operations

--- src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayroll/EmployeePayrollService.java
@@ -1,22 +1,22 @@
+package com.bridgelabz.employeepayroll;
 import java.sql.*;
 
 public class EmployeePayrollService {
     public static void main(String[] args) {
 
-        String query = "SELECT * FROM employee_payroll WHERE start BETWEEN ? AND ?";
+        String query = "SELECT COUNT(*), SUM(salary), AVG(salary), MIN(salary), MAX(salary) FROM employee_payroll";
 
         try (Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/employee_payroll_db", "root", "Shiva@123");
-             PreparedStatement ps = con.prepareStatement(query)) {
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(query)) {
 
-            ps.setDate(1, Date.valueOf("2023-01-01"));
-            ps.setDate(2, Date.valueOf("2024-12-31"));
-
-            ResultSet rs = ps.executeQuery();
-
-            System.out.println("Employees in Date Range:");
-            while (rs.next()) {
-                System.out.println(rs.getString("name"));
+            if (rs.next()) {
+                System.out.println("Count: " + rs.getInt(1));
+                System.out.println("Sum: ₹" + rs.getDouble(2));
+                System.out.println("Avg: ₹" + rs.getDouble(3));
+                System.out.println("Min: ₹" + rs.getDouble(4));
+                System.out.println("Max: ₹" + rs.getDouble(5));
             }
 
         } catch (Exception e) {
